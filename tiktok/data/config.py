480e0aba01aafcc3eff5c9befedd4f8e5058d9a5
mukhammadqodir <98277057+mukhammadqodir-alijonov@users.noreply.github.com>
2023-02-15 16:01:07 +0500

    Update config.py

--- tiktok/data/config.py
+++ tiktok/data/config.py
@@ -5,6 +5,6 @@ env = Env()
 env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = ${{secrets.TOKEN}}  # Bot toekn
+BOT_TOKEN = "5730948899:AAFomYIJoHljcZZ_H-8chfdk8tJau1ZKFUs"  # Bot toekn
 ADMINS = [1755665750]  # adminlar ro'yxati
 #IP = env.str("ip")  # Xosting ip manzili
